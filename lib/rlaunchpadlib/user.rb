350c1eb09f53c30f7b5a4b7ecf04eb886cbf54b9
Charles Butler <chuck@dasroot.net>
2014-05-18 16:45:25 -0400

    Extends user model with subresources * archive subscriptions * branches * bug subscriber packages * bugs * merge proposals * requested reviews

--- lib/rlaunchpadlib/user.rb
+++ lib/rlaunchpadlib/user.rb
@@ -19,6 +19,31 @@ module Rlaunchpadlib
             end
         end
 
+        def archive_subscriptions
+            @client.get(@username, 'getArchiveSubscriptionUrls')
+        end
+
+        def branches
+            @client.get(@username, 'getBranches')
+        end
+
+        def bug_subscriber_packages
+            @client.get(@username, 'getBugSubscriberPackages')
+        end
+
+        def merge_proposals
+            @client.get(@username, 'getMergeProposals')
+        end
+
+        def requested_reviews
+            @client.get(@username, 'getRequestedReviews')
+        end
+
+        def bugs
+            @client.get(@username, 'searchTasks')
+        end
+
+
         # I'm nuts so lets patch method missing for easy acces to 
         # profile data.
         def method_missing(name, *args, &block)
--- spec/rlaunchpadlib/lib/user_spec.rb
+++ spec/rlaunchpadlib/lib/user_spec.rb
@@ -16,10 +16,6 @@ describe 'Rlaunchpadlib::User' do
             VCR.eject_cassette
         end
 
-        it "records the fixture" do
-            @user.profile
-        end
-
         it "returns profile data as a hash" do
             expect(@user.profile).to be_a(Hash)
         end
@@ -29,6 +25,97 @@ describe 'Rlaunchpadlib::User' do
         end
 
     end
+
+    describe "GET user Archive Subscriptions" do
+
+        before do
+            VCR.insert_cassette 'user_archivesubscriptions', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.archive_subscriptions).to be_a(Hash)
+        end
+    end
+
+    describe "GET User Branches" do
+
+        before do
+            VCR.insert_cassette 'user_branches', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.branches).to be_a(Hash)
+        end
+    end
+
+    describe "GET User Bug Subscriber Packages" do
+
+        before do
+            VCR.insert_cassette 'user_bug_subscriber_packages', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.bug_subscriber_packages).to be_a(Hash)
+        end
+    end
+
+    describe "GET User Merge Proposals" do
+
+        before do
+            VCR.insert_cassette 'user_merge_proposals', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.merge_proposals).to be_a(Hash)
+        end
+    end
+
+    describe "GET User Requested Reviews" do
+
+        before do
+            VCR.insert_cassette 'user_requested_reviews', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.requested_reviews).to be_a(Hash)
+        end
+    end
+
+    describe "GET User Bugs" do
+
+        before do
+            VCR.insert_cassette 'user_bugs', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "returns a hash of Archive Subscriptions" do
+            expect(@user.bugs).to be_a(Hash)
+        end
+    end
+
 
     describe "Dynamic Method Attributes" do
 
