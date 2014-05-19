19f5917720bcfdca02ea4254aca7f806a118890e
Charles Butler <chuck@dasroot.net>
2014-05-18 20:05:42 -0400

    Adds subresources to project

--- lib/rlaunchpadlib/project.rb
+++ lib/rlaunchpadlib/project.rb
@@ -4,6 +4,12 @@ module Rlaunchpadlib
         attr_accessor :project
         attr_accessor :overview_data
 
+        attr_accessor :bug_data
+        attr_accessor :branch_data
+        attr_accessor :merge_proposal_data
+        attr_accessor :subscription_data
+        attr_accessor :timeline_data
+
         def initialize(project)
             @project = project
             @client = Rlaunchpadlib::Client.new
@@ -19,8 +25,52 @@ module Rlaunchpadlib
 
 
         def bugs
-            @client.get(@project, 'searchTasks')
+            if @bug_data.nil?
+                @bug_data = @client.get(@project, 'searchTasks')
+            end
+            @bug_data
         end
+
+
+        def branches
+            if @branch_data.nil?
+                @branch_data = @client.get(@project, 'getBranches')
+            end
+            @branch_data
+        end
+
+
+        def merge_proposals
+            if @merge_proposal_data.nil?
+                @merge_proposal_data = @client.get(@project, 'getMergeProposals')
+            end
+            @merge_proposal_data
+        end
+
+        def subscriptions
+            if @subscription_data.nil?
+                @subscription_data = @client.get(@project, 'getSubscriptions')
+            end
+            @subscription_data
+        end
+
+        def timeline
+            if @timeline_data.nil?
+                @timeline_data = @client.get(@project, 'get_timeline')
+            end
+            @timeline_data
+        end
+
+
+        def clear_cache
+            @overview_data = nil
+            @subscription_data = nil
+            @branch_data = nil
+            @merge_proposal_data = nil
+            @bug_data = nil
+            @timeline_data = nil
+        end
+
 
 
         # I'm nuts so lets patch method missing.
--- spec/rlaunchpadlib/lib/project_spec.rb
+++ spec/rlaunchpadlib/lib/project_spec.rb
@@ -27,7 +27,7 @@ describe 'Rlaunchpadlib::Project' do
     end
 
 
-    describe "GET Subresource of Project Bugs" do
+    describe "Subresource of Project Bugs" do
 
         before do
             VCR.insert_cassette 'project_bugs', :record => :new_episodes
@@ -43,6 +43,92 @@ describe 'Rlaunchpadlib::Project' do
 
     end
 
+    describe "Subresource of Project Branches" do
+
+        before do
+            VCR.insert_cassette 'project_branches', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "Returns a Hash of all Branches" do
+            expect(@project.branches).to be_a(Hash)
+        end
+
+    end
+
+
+    describe "Subresource of Project Merge Proposals" do
+
+        before do
+            VCR.insert_cassette 'project_merge_proposals', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "Returns a Hash of all Merge Proposals" do
+            expect(@project.merge_proposals).to be_a(Hash)
+        end
+
+    end
+
+
+    describe "Subresource of Project Subscriptions" do
+
+        before do
+            VCR.insert_cassette 'project_subscriptions', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "Returns a Hash of all Subscriptions" do
+            expect(@project.subscriptions).to be_a(Hash)
+        end
+
+    end
+
+
+    describe "Subresource of Project Timeline" do
+
+        before do
+            VCR.insert_cassette 'project_timeline', :record => :new_episodes
+        end
+
+        after do
+            VCR.eject_cassette
+        end
+
+        it "Returns a Hash of the Timeline" do
+            expect(@project.timeline).to be_a(Hash)
+        end
+
+    end
+
+
+     describe "Reset Cache" do
+        it "clears all data storage attributes" do
+            # warm the caches
+            @project.subscription_data = Hash.new
+            @project.merge_proposal_data = Hash.new
+            @project.branch_data = Hash.new
+            @project.overview_data = Hash.new
+            @project.bug_data = Hash.new
+            @project.timeline_data = Hash.new
+            # expunge the cache
+            @project.clear_cache
+            expect(@project.subscription_data).to be_nil
+            expect(@project.merge_proposal_data).to be_nil
+            expect(@project.branch_data).to be_nil
+            expect(@project.overview_data).to be_nil
+            expect(@project.timeline_data).to be_nil
+        end
+    end
 
 
     describe "Dynamic Method Attributes" do
